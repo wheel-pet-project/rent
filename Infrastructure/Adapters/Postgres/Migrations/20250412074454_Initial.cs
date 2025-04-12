e1e69a025a74a22e759386fbc10982f614211dbb
Nikita Berezhko <berezhko.nik16@gmail.com>
2025-04-12 10:50:07 +0300

    replace initial migration

---
+++ Infrastructure/Adapters/Postgres/Migrations/20250412074454_Initial.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Adapters.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "inbox",
+                columns: table => new
+                {
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    occurred_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    processed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_inbox", x => x.event_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "level",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    needed_points = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_level", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "outbox",
+                columns: table => new
+                {
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    occurred_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    processed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_outbox", x => x.event_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "status",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_status", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicle_model",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    price_per_minute = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    price_per_hour = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    price_per_day = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehicle_model", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    level_id = table.Column<int>(type: "integer", nullable: false),
+                    rents = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_level_id",
+                        column: x => x.level_id,
+                        principalTable: "level",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicle",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    vehicle_model_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehicle", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_vehicle_model_id",
+                        column: x => x.vehicle_model_id,
+                        principalTable: "vehicle_model",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "booking",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    vehicle_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_booking", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_customer_id",
+                        column: x => x.customer_id,
+                        principalTable: "customer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_vehicle_id",
+                        column: x => x.vehicle_id,
+                        principalTable: "vehicle",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rent",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    status_id = table.Column<int>(type: "integer", nullable: false),
+                    booking_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    vehicle_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    price_per_minute = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    price_per_hour = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    price_per_day = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    actual_amount = table.Column<decimal>(type: "numeric", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rent", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_booking_id",
+                        column: x => x.booking_id,
+                        principalTable: "booking",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_customer_id",
+                        column: x => x.customer_id,
+                        principalTable: "customer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_status_id",
+                        column: x => x.status_id,
+                        principalTable: "status",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_vehicle_id",
+                        column: x => x.vehicle_id,
+                        principalTable: "vehicle",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "level",
+                columns: new[] { "id", "name", "needed_points" },
+                values: new object[,]
+                {
+                    { 1, "fickle", 1 },
+                    { 2, "regular", 100 },
+                    { 3, "frequent", 300 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "status",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1, "inprogress" },
+                    { 2, "completed" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_booking_customer_id",
+                table: "booking",
+                column: "customer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_booking_vehicle_id",
+                table: "booking",
+                column: "vehicle_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_level_id",
+                table: "customer",
+                column: "level_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_inbox_messages_unprocessed",
+                table: "inbox",
+                columns: new[] { "occurred_on_utc", "processed_on_utc" },
+                filter: "processed_on_utc IS NULL")
+                .Annotation("Npgsql:IndexInclude", new[] { "event_id", "type" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_outbox_messages_unprocessed",
+                table: "outbox",
+                columns: new[] { "occurred_on_utc", "processed_on_utc" },
+                filter: "processed_on_utc IS NULL")
+                .Annotation("Npgsql:IndexInclude", new[] { "event_id", "type" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rent_booking_id",
+                table: "rent",
+                column: "booking_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rent_customer_id",
+                table: "rent",
+                column: "customer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rent_status_id",
+                table: "rent",
+                column: "status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rent_vehicle_id",
+                table: "rent",
+                column: "vehicle_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vehicle_vehicle_model_id",
+                table: "vehicle",
+                column: "vehicle_model_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "inbox");
+
+            migrationBuilder.DropTable(
+                name: "outbox");
+
+            migrationBuilder.DropTable(
+                name: "rent");
+
+            migrationBuilder.DropTable(
+                name: "booking");
+
+            migrationBuilder.DropTable(
+                name: "status");
+
+            migrationBuilder.DropTable(
+                name: "customer");
+
+            migrationBuilder.DropTable(
+                name: "vehicle");
+
+            migrationBuilder.DropTable(
+                name: "level");
+
+            migrationBuilder.DropTable(
+                name: "vehicle_model");
+        }
+    }
+}
