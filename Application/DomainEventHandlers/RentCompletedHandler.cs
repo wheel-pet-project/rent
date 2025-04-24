6db7c6e7bf0e12fb56e99b61348d2270248bc4ed
Nikita Berezhko <berezhko.nik16@gmail.com>
2025-04-24 19:17:19 +0300

    added updating points in loyal program after rent completion

--- Application/DomainEventHandlers/RentCompletedHandler.cs
+++ Application/DomainEventHandlers/RentCompletedHandler.cs
@@ -1,14 +1,33 @@
 using Application.Ports.Kafka;
+using Application.Ports.Postgres;
+using Application.Ports.Postgres.Repositories;
 using Domain.RentAggregate.DomainEvents;
+using Domain.SharedKernel.Errors;
+using Domain.SharedKernel.Exceptions.DataConsistencyViolationException;
 using MediatR;
 
 namespace Application.DomainEventHandlers;
 
-public class RentCompletedHandler(IMessageBus messageBus) : INotificationHandler<RentCompletedDomainEvent>
+public class RentCompletedHandler(
+    ICustomerRepository customerRepository,
+    IUnitOfWork unitOfWork,
+    IMessageBus messageBus) : INotificationHandler<RentCompletedDomainEvent>
 {
     public async Task Handle(RentCompletedDomainEvent domainEvent, CancellationToken cancellationToken)
     {
-        // todo: add updating level in loyalty program
+        var customer = await customerRepository.GetById(domainEvent.CustomerId);
+        if (customer == null) throw new DataConsistencyViolationException(
+            $"Customer with id {domainEvent.CustomerId} not found");
+        
         await messageBus.Publish(domainEvent, cancellationToken);
+        
+        customer.AddRent();
+        
+        if (customer.Level.IsNeededChange(customer.Points)) customer.ChangeToOneLevel();
+        
+        customerRepository.Update(customer);
+        
+        var commitResult = await unitOfWork.Commit();
+        if (commitResult.IsFailed) throw ((CommitFail)commitResult.Errors[0]).Exception;
     }
 }
---
+++ UnitTests/Application/DomainEventHandlers/RentCompletedHandlerShould.cs
@@ -0,0 +1,78 @@
+using Application.DomainEventHandlers;
+using Application.Ports.Kafka;
+using Application.Ports.Postgres;
+using Application.Ports.Postgres.Repositories;
+using Domain.CustomerAggregate;
+using Domain.RentAggregate.DomainEvents;
+using Domain.SharedKernel.Errors;
+using Domain.SharedKernel.Exceptions.DataConsistencyViolationException;
+using FluentResults;
+using JetBrains.Annotations;
+using Moq;
+using Xunit;
+
+namespace UnitTests.Application.DomainEventHandlers;
+
+[TestSubject(typeof(RentCompletedHandler))]
+public class RentCompletedHandlerShould
+{
+    private readonly RentCompletedDomainEvent _event = new(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(),
+        Guid.NewGuid(), 100);
+    
+    private readonly Customer _customer = Customer.Create(Guid.NewGuid());
+    
+    private readonly Mock<IMessageBus> _messageBusMock = new();
+    private readonly Mock<ICustomerRepository> _customerRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+
+    private readonly RentCompletedHandler _handler;
+    
+    public RentCompletedHandlerShould()
+    {
+        _customerRepositoryMock.Setup(x => x.GetById(It.IsAny<Guid>())).ReturnsAsync(_customer);
+        _unitOfWorkMock.Setup(x => x.Commit()).ReturnsAsync(Result.Ok);
+
+        _handler = new RentCompletedHandler(_customerRepositoryMock.Object, _unitOfWorkMock.Object,
+            _messageBusMock.Object);
+    }
+
+    [Fact]
+    public async Task ThrowDataConsistencyExceptionIfCustomerNotFound()
+    {
+        // Arrange
+        _customerRepositoryMock.Setup(x => x.GetById(It.IsAny<Guid>())).ReturnsAsync(null as Customer);
+
+        // Act
+        async Task Act() => await _handler.Handle(_event, TestContext.Current.CancellationToken);
+
+        // Assert
+        await Assert.ThrowsAsync<DataConsistencyViolationException>(Act);
+    }
+    
+    [Fact]
+    public async Task CallMessageBusPublish()
+    {
+        // Arrange
+
+        // Act
+        await _handler.Handle(_event, TestContext.Current.CancellationToken);
+
+        // Assert
+        _messageBusMock.Verify(
+            x => x.Publish(It.IsAny<RentCompletedDomainEvent>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task ThrowExceptionInCommitErrorIfCommitFailed()
+    {
+        // Arrange
+        _unitOfWorkMock.Setup(x => x.Commit()).ReturnsAsync(Result.Fail(new CommitFail("", new DataConsistencyViolationException())));
+
+        // Act
+        async Task Act() => await _handler.Handle(_event, TestContext.Current.CancellationToken);
+
+        // Assert
+        await Assert.ThrowsAsync<DataConsistencyViolationException>(Act);
+    }
+}
